d90d9472770476bbeaa94b4218f7430694954738
Dibakar Sigdel <Vasuji@users.noreply.github.com>
2019-03-17 21:29:41 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,13 @@
 module.exports = {
-	urlSubjectViews: 'mongodb://localhost:27017/donorschoose',
+		
+//1. use this line with USER:PASSWARD when using cloud (AWS)
+urlSubjectViews: 'mongodb://USER:PASSWORD@localhost:27017/donorschoose?authSource=admin';
+
+//2. Use this when using MongoAtlas
+//urlSubjectViews: "mongodb+srv://USER:PASSWORD@cluster0-ui9hr.mongodb.net/test?retryWrites=true";
+
+
+//3. use this line if using PC
+//urlSubjectViews: 'mongodb://localhost:27017/donorschoose',
 
 }
