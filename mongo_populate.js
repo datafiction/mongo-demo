57f3ee2a7e6b8b30b5eb979e004d54a24c81baf5
Dibakar Sigdel <Vasuji@users.noreply.github.com>
2019-03-17 21:28:15 -0700

    Update mongo_populate.js

--- mongo_populate.js
+++ mongo_populate.js
@@ -1,7 +1,18 @@
-
 var MongoClient = require('mongodb').MongoClient;
-//Create a database named "mydb":
+
+
+//1. use this line with USER:PASSWARD when using cloud (AWS)
 var url = 'mongodb://USER:PASSWORD@localhost:27017/donorschoose?authSource=admin';
+
+//2. Use this when using MongoAtlas
+//var url = "mongodb+srv://USER:PASSWORD@cluster0-ui9hr.mongodb.net/test?retryWrites=true";
+
+
+//3. use this line if using PC
+//urlSubjectViews: 'mongodb://localhost:27017/donorschoose',
+
+
+
 
 
 var samples = require('./sampledata.json');
@@ -50,11 +61,3 @@ MongoClient.connect(url, function(err, db) {
 	console.log("all items inserted to database");
 	db.close();
 });
-
-
-
-
-
-
-
-
